20f45b492e943f6159717074a59afe87363c4d6e
fedorfelician <62263656+fedorfelician@users.noreply.github.com>
2020-11-27 09:05:41 +0100

    evarvdf

--- contact.php
+++ contact.php
@@ -23,7 +23,7 @@ $okMessage = 'Contact form successfully submitted. Thank you, I will get back to
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 // ReCaptch Secret
-$recaptchaSecret = '6LfKURIUAAAAAKEPdFXGUiRsQYtEYUnH1-OB5Mgx';
+$recaptchaSecret = '6LdDP-8ZAAAAADPlrzRLci_peOifNlm6sHGhF5id';
 
 // let's do the sending
 
