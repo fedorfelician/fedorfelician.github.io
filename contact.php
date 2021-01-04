1ea459ac212b4cf28082bcaa8b3a488815ec95d8
fedorfelician <62263656+fedorfelician@users.noreply.github.com>
2021-01-04 21:14:21 +0100

    Revert "3h5"
    This reverts commit 56b3784095878dc0d76b1e1ea7f46ac60ff26dfa.

--- contact.php
+++ contact.php
@@ -1,90 +1,89 @@
 <?php
+
+//  CONFIGURE EVERYTHING HERE
+
 // require ReCaptcha class
 require('recaptcha-master/src/autoload.php');
 
-// configure
-// an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+// "innen érkezik" az email
+$from = 'Céges email <demo@domain.com>';
 
-// an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+// email cím, amire a form kimenete érkezik
+$sendTo = 'Fedor Felicián <fedor.felician@gmail.com>';
 
-// subject of the email
-$subject = 'New message from contact form';
+// email tárgya
+$subject = 'Új üzenet érkezett a weboldalról';
 
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+// form mezőnevek és fordításaik // array variable name => Text to appear in the email
+$fields = array('name' => 'Név', 'email' => 'Email', 'phone' => 'Telefonszám', 'message' => 'Üzenet'); 
 
-// message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+// oké üzenet
+$okMessage = 'Üzenet elküldve. Köszönjük!';
 
-// If something goes wrong, we will display this message.
-$errorMessage = 'There was an error while submitting the form. Please try again later';
+// hibaüzenet
+$errorMessage = 'Hiba az üzenet küldésében, kérjük próbálja újra később.';
 
 // ReCaptch Secret
-$recaptchaSecret = '6LdDP-8ZAAAAADPlrzRLci_peOifNlm6sHGhF5id';
+$recaptchaSecret = '6LeNZPcZAAAAAJ34Q_GubW0CrNHbTCQGbvNw8lve';
 
-// let's do the sending
+//  LET'S DO THE SENDING
 
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
+ // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+//error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(0);
 
-try {
-    if (!empty($_POST)) {
-
-        // validate the ReCaptcha, if something is wrong, we throw an Exception,
+try
+{
+    //if(count($_POST) == 0) throw new \Exception('Form is empty');
+    // validate the ReCaptcha, if something is wrong, we throw an Exception,
         // i.e. code stops executing and goes to catch() block
         
-        if (!isset($_POST['g-recaptcha-response'])) {
-            throw new \Exception('ReCaptcha is not set.');
-        }
-
-        // do not forget to enter your secret key from https://www.google.com/recaptcha/admin
-        
-        $recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());
-        
-        // we validate the ReCaptcha field together with the user's IP address
-        
-        $response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-
-        if (!$response->isSuccess()) {
-            throw new \Exception('ReCaptcha was not validated.');
-        }
-        
-        // everything went well, we can compose the message, as usually
-        
-        $emailText = "You have a new message from your contact form\n=============================\n";
-
-        foreach ($_POST as $key => $value) {
-            // If the field exists in the $fields array, include it in the email
-            if (isset($fields[$key])) {
-                $emailText .= "$fields[$key]: $value\n";
-            }
-        }
-    
-        // All the neccessary headers for the email.
-        $headers = array('Content-Type: text/plain; charset="UTF-8";',
-            'From: ' . $from,
-            'Reply-To: ' . $from,
-            'Return-Path: ' . $from,
-        );
-        
-        // Send email
-        mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
-        $responseArray = array('type' => 'success', 'message' => $okMessage);
+    if (!isset($_POST['g-recaptcha-response'])) {
+        throw new \Exception('ReCaptcha is not set.');
     }
-} catch (\Exception $e) {
-    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+
+    // do not forget to enter your secret key from https://www.google.com/recaptcha/admin
+    $recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());
+
+    // we validate the ReCaptcha field together with the user's IP address
+    $response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+    if (!$response->isSuccess()) {
+        throw new \Exception('ReCaptcha was not validated.');
+    }
+   
+    $emailText = "Új üzenet érkezett a weboldal kapcsolat menüpontján keresztül\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+    
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
+// if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
-
     header('Content-Type: application/json');
-
     echo $encoded;
-} else {
+}
+// else just display the message
+else {
     echo $responseArray['message'];
 }
